ba3cf53816737c5a6800603fb4d3464bee98dfd0
Sergey Davydov <webmaster@sdcollection.com>
2020-02-18 23:45:28 +0300

    add version tag if possible

--- git-comit-version.sh
+++ git-comit-version.sh
@@ -3,13 +3,16 @@
 # git commit founded versions and descriptions
 
 pattern="(version[\:\s]+(\d+\.?)+|description\:)"
+pattern2="([\d]+[\.]?)+"
 foundedVersions=`grep  -iP "$pattern" *.php`
+version=`echo "$foundedVersions"  | grep -Po "$pattern2"`
+
+
 echo "Founded Versions and Descriptions by 
 Pattern: $pattern 
 
 Result to commit message:
 $foundedVersions
-
 "
 
 git add *
@@ -17,4 +20,21 @@ git add -u
 git commit -m "$foundedVersions"
 
 
+if [ ! -z "$version" ]
+then
+echo "Possible add tag:
+"
+ read -p " > git tag 'v$version' [y/n]? " -n 1 -r
+ echo    # (optional) move to a new line
+ if [[ $REPLY =~ ^[Yy]$ ]]
+ then
+  git tag "v$version"
+ fi
+fi
 
+ read -p "git push [y/n]? " -n 1 -r
+ echo    # (optional) move to a new line
+ if [[ $REPLY =~ ^[Yy]$ ]]
+ then
+  git push --tags --set-upstream origin
+ fi
