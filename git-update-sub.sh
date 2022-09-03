15583e27a596eb6f1effac450e962498b06143db
Sergey Davydov <dev@sdcollection.com>
2022-09-03 17:04:00 +0300

    fetch before update "FastForward-only", It still needs check manually, but in locally updated repo

--- git-update-sub.sh
+++ git-update-sub.sh
@@ -1,1 +1,1 @@
- find -L ./ -name .git -printf "\n%h\n" -execdir git pull --ff-only --all -v --progress \;
+ find -L ./ -name .git -printf "\n%h\n" -execdir git fetch --all -v --progress \; -execdir git pull --ff-only --tag --all \;
