0c7671ee0d03d542f7bb60b7d04474b0700c4e77
Sergey Davydov <dev@sdcollection.com>
2023-01-29 09:46:44 +0300

    can show only 'changed'

--- git-update-sub.sh
+++ git-update-sub.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+if [ "$1" == "changed" ]; then
+ $0 2>&1 |  grep -vE '\[up to date\]|^Fetching origin$|^From.*$'
+else
  find -L ./ -name .git -printf "\n%h\n" -execdir git fetch --all -v --progress \; -execdir git pull --ff-only --tag --all \;
+fi
