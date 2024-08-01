5e8dcacd69787f5cb55819f5055843eec9ac5f04
Sergey Davydov <sddevelop@ya.ru>
2024-08-01 20:11:11 +0300

    fix news date

--- git-commit-newest-date.sh
+++ git-commit-newest-date.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # commit with date of newest file
 
-newdate=$(stat -c "%y" * | sort -n -k 2 | head -1)
+newdate=$(lc_all=en stat -c "%y" * | sort -nk1r -nk2r| head -1)
 
 GIT_AUTHOR_DATE=$newdate GIT_COMMITTER_DATE=$newdate git commit --date="$newdate"  "$@"
